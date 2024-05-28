f958201183e3566d57c3ee9e64b544922fc52a99
Matheus Silvestre <74218849+silvestre149-bit@users.noreply.github.com>
2024-05-28 20:42:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ OBR.onReady(() => {
     let tokenWithPatch = 'id-do-token-com-tapa-olho';
     let tokenWithoutPatch = 'id-do-token-sem-tapa-olho';
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'sua-tecla') {
-            const newTokenId = currentTokenId === tokenWithPatch ? tokenWithoutPatch : tokenWithPatch;
-            OBR.assets.updateAsset({
-                id: currentTokenId,
-                assetId: newTokenId
-            });
-            currentTokenId = newTokenId;
-        }
+    document.getElementById('toggleTokenButton').addEventListener('click', () => {
+        const newTokenId = currentTokenId === tokenWithPatch ? tokenWithoutPatch : tokenWithPatch;
+        OBR.assets.updateAsset({
+            id: currentTokenId,
+            assetId: newTokenId
+        });
+        currentTokenId = newTokenId;
     });
 });
+
