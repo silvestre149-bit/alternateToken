159ad4cc77cb2585b5b31ccf54631f9fa75426b5
Matheus Silvestre <74218849+silvestre149-bit@users.noreply.github.com>
2024-05-28 21:27:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 OBR.onReady(() => {
-    let currentTokenId = 'id-do-token-com-tapa-olho';
-    let tokenWithPatch = 'id-do-token-com-tapa-olho';
-    let tokenWithoutPatch = 'id-do-token-sem-tapa-olho';
+    let currentTokenId = '9e098ab3-f059-44ad-be1c-e903c5877579';
+    let tokenWithPatch = '9e098ab3-f059-44ad-be1c-e903c5877579';
+    let tokenWithoutPatch = '08f5e40e-6241-4b83-b478-41c13938bfde';
 
     document.getElementById('toggleTokenButton').addEventListener('click', () => {
         const newTokenId = currentTokenId === tokenWithPatch ? tokenWithoutPatch : tokenWithPatch;
